4c755627fe211eee85539fb9a57d7405a32e5fce
Sigmanificient <edhyjox@gmail.com>
2023-10-02 17:45:41 +0200

    Add package for my-epitech-relay

--- flake.nix
+++ flake.nix
@@ -1,9 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
+
+    my-epitech-relay-git = {
+      url = "github:norech/my-epitech-relay/master";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { nixpkgs, my-epitech-relay-git, ... }:
     let
       forAllSystems = function:
         nixpkgs.lib.genAttrs [
@@ -23,6 +28,41 @@
             chromium
           ];
         };
+      });
+
+      packages = forAllSystems (pkgs: rec {
+        my-epitech-relay = pkgs.buildNpmPackage rec {
+          name = "my-epitech-relay";
+          version = "1.0.0";
+
+          src = my-epitech-relay-git;
+          npmDepsHash = "sha256-r6IBNmzBEhmo2/nUroYrlpRz9RwceL3nm9rzRWOwUaA=";
+
+          buildInputs = with pkgs; [ nodejs_20 ];
+          PUPPETEER_SKIP_DOWNLOAD = "true";
+
+          installPhase = ''
+            mkdir -p $out/bin
+
+            npm run build
+
+            cp out/index.js $out/index.js
+            cp out/cookies.js $out/cookies.js
+
+            cp -r node_modules $out/node_modules
+
+            cat <<EOF > $out/bin/${name}
+            #!${pkgs.runtimeShell}
+
+            export BROWSER_BINARY_PATH=${pkgs.chromium}/bin/chromium
+            ${pkgs.nodejs}/bin/node $out/index.js
+            EOF
+
+            chmod +x $out/bin/${name}
+          '';
+        };
+
+        default = my-epitech-relay;
       });
     };
 }
